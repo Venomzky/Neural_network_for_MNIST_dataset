52997b3aff0a3d2b5cb8e0e0bc07080e0afaf587
Venomzky <venomskydev@gmail.com>
2023-08-25 17:44:24 +0200

    added droput to hidden layer and batching

--- NL_MNIST.py
+++ NL_MNIST.py
@@ -9,11 +9,12 @@ relu2deriv = lambda x: x>=0
 images = (x_train[0:1000].reshape(1000,28*28))/255     #pixel value of images reshaped into 28*28 vectors, each pixel value divided by 255
 labels = y_train[0:1000]                               #label for each image
 
-hidden_size = 40
-alpha = 0.005
+hidden_size = 100
+alpha = 0.001
 pixels_per_image = 784
 num_labels = 10
-iteration = 50 #not too many iterations to not overfit 
+iteration = 300 #not too many iterations to not overfit 
+batch_size = 100
 np.random.seed(1)
 weights_0_1 = 0.2*np.random.random((pixels_per_image, hidden_size)) - 0.1
 weights_1_2 = 0.2*  np.random.random((hidden_size,num_labels)) - 0.1
@@ -36,30 +37,30 @@ for i,l in enumerate(y_test):
 for j in range(iteration):
     correct_cnt = 0
     error = 0.0
-    for i in range(len(images)):
-        layer0 = images[i:i+1]
+    for i in range(int(len(images)/batch_size)):
+        batch_start, batch_end = ((i*batch_size), ((i+1)*batch_size))
+
+        layer0 = images[batch_start:batch_end]
         layer1 = relu(np.dot(layer0,weights_0_1))
+        dropout_mask = np.random.randint(2, size=layer1.shape)
+        layer1 *= dropout_mask * 2 #50% of nodes are turned off, value of rest nodes are increased by 1/percent of turned on nodes
         layer2 = np.dot(layer1,weights_1_2)
-        error += np.sum((labels[i:i+1]-layer2)**2)
-        correct_cnt += int(np.argmax(layer2)==np.argmax(labels[i:i+1]))
+        error += np.sum((labels[batch_start:batch_end]-layer2)**2)
+        for k in range(batch_size):
+            correct_cnt += int(np.argmax(layer2[k:k+1])==np.argmax(labels[batch_start+k:batch_start+k+1]))
+            layer_2_delta = (labels[batch_start:batch_end]-layer2)/batch_size
+            layer_1_delta = layer_2_delta.dot(weights_1_2.T)* relu2deriv(layer1)
+            layer_1_delta *= dropout_mask
 
-        layer2_delta = (labels[i:i+1]-layer2)
-        layer1_delta = layer2_delta.dot(weights_1_2.T)*relu2deriv(layer1)
-        
-        weights_1_2 += alpha * layer1.T.dot(layer2_delta)
-        weights_0_1 += alpha * layer0.T.dot(layer1_delta)
-print("training results")
-print(f"error: {error/len(images)} acc:{correct_cnt/len(images)}")
-
-
-
-error = 0.0
-correct_cnt = 0
-for i in range(len(test_images)):
-    layer_0 = test_images[i:i+1]
-    layer_1 = relu(np.dot(layer_0,weights_0_1))
-    layer_2 = np.dot(layer_1,weights_1_2)
-    error += np.sum((test_labels[i:i+1] - layer_2) ** 2)
-    correct_cnt += int(np.argmax(layer_2) == np.argmax(test_labels[i:i+1]))
-print("results on unseen dataset")
-print(f"error: {error/len(test_images)} acc:{correct_cnt/len(test_images)}")
+            weights_1_2 += alpha * layer1.T.dot(layer_2_delta)
+            weights_0_1 += alpha * layer0.T.dot(layer_1_delta)
+    if (j%10==0):
+        test_error = 0.0
+        test_correct_cnt = 0
+        for i in range(len(test_images)):
+            layer0 = test_images[i:i+1]
+            layer1 = relu(np.dot(layer0,weights_0_1))
+            layer2 = np.dot(layer1,weights_1_2)
+            test_error += np.sum((test_labels[i:i+1] - layer2) ** 2)
+            test_correct_cnt += int(np.argmax(layer2) == np.argmax(test_labels[i:i+1]))
+        print(f"I: {j} Train error: {error/len(images)} Train acc: {correct_cnt/len(images)} Test error: {test_error/len(test_images)} Test acc: {test_correct_cnt/len(test_images)}")
